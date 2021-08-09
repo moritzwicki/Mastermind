39243ac5397ea8b6a7afa2dcd8f2ee0003aace16
Moritz Wicki <wickimoritz09@gmail.com>
2021-08-09 13:54:02 +0200

    added board

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MastermindService} from "../mastermind.service";
+import {Colors} from "../colors.type";
 
 @Component({
   selector: 'app-board',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public masterMindService: MastermindService) { }
+
+  rounds = [12,11,10,9,8,7,6,5,4,3,2,1];
+  cols = [1,2,3,4];
 
   ngOnInit(): void {
+  }
+
+  changeColor(col: number, row: number, colourNow: string) {
+
   }
 
 }
